18239a715bb4eb1a3fa258f485cbb0ccb3aae5c8
Javier_G <>
2020-11-30 10:54:18 +0100

    implementacion de sockets

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import { usersRoutes } from './routes/user.routes';
 import { bookRoutes } from './routes/book.routes'
 import { authorRoutes } from './routes/author.routes'
 import { authRouter } from './routes/auth.routes';
+import socketio from "socket.io";
+import path from "path";
 
 const app = express();
 
@@ -10,7 +12,31 @@ app.use(express.json());
 
 app.set('port', 8080);
 
+app.get("/", (req: any, res: any ) => {
+    res.sendFile(path.resolve("./index.html"));
+});
+
 app.listen(app.get('port'), () => console.log("Server is running in port 8080"));
+
+
+const server = app.listen(app.get("port"), () => {
+    console.log(`listening on ${app.get('port')}`);
+    
+});
+
+let io = new socketio.Server(server);
+
+io.on("connection", (socket:any) => {
+
+    console.log("An user connected");
+
+    socket.on('message', (message: any) => {
+
+        io.emit('message', message);
+
+    })
+    
+});
 
 app.use('/users', usersRoutes.router );
 app.use('/books', bookRoutes.router );
